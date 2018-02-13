44ded4a3c46c347cd3147f7baaab24a12f1d05c7
chen,zewei <scottchen@ScottdeMacBook-Pro.local>
2018-02-12 22:47:22 -0500

    added login & register controllers

--- cs4500-spring2018-team34/src/main/webapp/config.js
+++ cs4500-spring2018-team34/src/main/webapp/config.js
@@ -6,10 +6,22 @@
     function Config ($routeProvider) {
         $routeProvider
                 .when('/', {
-                    templateUrl: 'views/user/homepage.html',
+                    templateUrl: 'views/user/home.view.client.html',
                     controller: 'HomeController',
                     controllerAs: 'model'
                 })
+                
+                .when('/login', {
+        				templateUrl: 'views/user/login.view.client.html',
+        				controller: 'LoginController',
+        				controllerAs: 'model'
+        			})
+        			
+        			.when('/register', {
+        				templateUrl: 'views/user/register.view.client.html',
+        				controller: 'RegisterController',
+        				controllerAs: 'model'
+        			})
     }
 
 })();
