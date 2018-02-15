f6031e4e3fb8eae3662fb70e7300267b991776ab
lixua <lixua@ccs.neu.edu>
2018-02-15 12:07:35 -0500

    XuanyuLi work

--- cs4500-spring2018-team34/src/main/java/edu/northeastern/cs4500/controllers/customer/CustomerRepository.java
+++ cs4500-spring2018-team34/src/main/java/edu/northeastern/cs4500/controllers/customer/CustomerRepository.java
@@ -1,10 +1,12 @@
 package edu.northeastern.cs4500.controllers.customer;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.query.Procedure;
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 
-public interface CustomerRepository extends CrudRepository<CustomerObject, Integer> {
+import java.util.List;
+
+public interface CustomerRepository extends JpaRepository<CustomerObject, Integer> {
     @Procedure(name = "custom_info")
-    String customer_info(@Param("username") String inParam1);
+    List<CustomerObject> customer_info(@Param("username") String inParam1);
 }
