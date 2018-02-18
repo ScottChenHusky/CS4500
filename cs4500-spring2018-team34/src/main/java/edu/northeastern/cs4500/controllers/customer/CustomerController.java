b3f9b694fc8dfc094e8106d2d0cc81f95a410b54
chen,zewei <scottchen@ScottdeMacBook-Pro.local>
2018-02-18 18:06:58 -0500

    before pull

--- cs4500-spring2018-team34/src/main/java/edu/northeastern/cs4500/controllers/customer/CustomerController.java
+++ cs4500-spring2018-team34/src/main/java/edu/northeastern/cs4500/controllers/customer/CustomerController.java
@@ -6,6 +6,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import edu.northeastern.cs4500.controllers.hello.HelloObject;
+
 import java.util.List;
 
 @RestController
