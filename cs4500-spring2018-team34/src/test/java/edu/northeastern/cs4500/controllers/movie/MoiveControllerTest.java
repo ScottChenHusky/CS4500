8dd389635e6aa272c794c804ff246bd912479dc2
chen,zewei <chen.zewe@husky.neu.edu>
2018-02-23 17:51:37 -0500

    added test coverage

--- cs4500-spring2018-team34/src/test/java/edu/northeastern/cs4500/controllers/movie/MoiveControllerTest.java
+++ cs4500-spring2018-team34/src/test/java/edu/northeastern/cs4500/controllers/movie/MoiveControllerTest.java
@@ -12,5 +12,14 @@ public class MoiveControllerTest {
 	    int i = m.hashCode();
 	    assertTrue(i == 0);
 	}
+	
+	@Test
+	public void equals() {
+		Movie m1 = new Movie();
+		Movie m2 = new Movie();
+		m1.setDirector("123");
+		m2.setDirector("123");
+		assertTrue(m1.equals(m2));
+	}
 
 }
