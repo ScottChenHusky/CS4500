0175f795b39330e1ca7f7d423c858167cd3261f3
lixua <lixua@ccs.neu.edu>
2018-02-23 18:01:36 -0500

    Test movie

--- cs4500-spring2018-team34/src/test/java/edu/northeastern/cs4500/controllers/movie/MovieTest.java
+++ cs4500-spring2018-team34/src/test/java/edu/northeastern/cs4500/controllers/movie/MovieTest.java
@@ -2,6 +2,7 @@ package edu.northeastern.cs4500.controllers.movie;// Created by xuanyuli on 2/23
 
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 public class MovieTest {
@@ -46,5 +47,40 @@ public class MovieTest {
     Movie movie2 = new Movie();
     movie2.id = 2;
     assertTrue(movie1.hashCode() != movie2.hashCode());
+  }
+
+  @Test
+  public void testName(){
+    Movie movie1 = new Movie();
+    movie1.setName("Test");
+    assertEquals(movie1.getName(), "Test");
+  }
+
+  @Test
+  public void testDate(){
+    Movie movie1 = new Movie();
+    movie1.setDate("1999-09-99");
+    assertEquals(movie1.getDate(), "1999-09-09");
+  }
+
+  @Test
+  public void testScore(){
+    Movie movie1 = new Movie();
+    movie1.setScore("1.1");
+    assertEquals(movie1.getScore(), "1.1");
+  }
+
+  @Test
+  public void testDescription(){
+    Movie movie1 = new Movie();
+    movie1.setDescription("Good");
+    assertEquals(movie1.getDescription(), "Good");
+  }
+
+  @Test
+  public void testLevel(){
+    Movie movie1 = new Movie();
+    movie1.setLevel("PG-13");
+    assertEquals(movie1.getLevel(), "PG-13");
   }
 }
