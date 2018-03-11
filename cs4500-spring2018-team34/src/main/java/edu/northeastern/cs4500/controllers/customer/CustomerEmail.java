6bc291ce0e8e068f41c12ebe719308740ee97c03
lixua <lixua@ccs.neu.edu>
2018-03-11 18:11:58 -0400

    Add Email Function

--- cs4500-spring2018-team34/src/main/java/edu/northeastern/cs4500/controllers/customer/CustomerEmail.java
+++ cs4500-spring2018-team34/src/main/java/edu/northeastern/cs4500/controllers/customer/CustomerEmail.java
@@ -1,7 +1,6 @@
 package edu.northeastern.cs4500.controllers.customer;
 
 import java.util.Properties;
-
 import javax.mail.Message;
 import javax.mail.MessagingException;
 import javax.mail.PasswordAuthentication;
@@ -11,22 +10,20 @@ import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 
 public class CustomerEmail {
-
   public static void main(String[] args) {
-
-    final String username = "spoiledtomatillos@gmail.com";
-    final String password = "SpoiledTomatillosSpoiledTomatillos";
-
     Properties props = new Properties();
+    props.put("mail.smtp.host", "smtp.gmail.com");
+    props.put("mail.smtp.socketFactory.port", "465");
+    props.put("mail.smtp.socketFactory.class",
+            "javax.net.ssl.SSLSocketFactory");
     props.put("mail.smtp.auth", "true");
-    props.put("mail.smtp.starttls.enable", "true");
-    props.put("mail.smtp.host", "stmp.gmail.com");
-    props.put("mail.smtp.port", "587");
+    props.put("mail.smtp.port", "465");
 
-    Session session = Session.getInstance(props,
+    Session session = Session.getDefaultInstance(props,
             new javax.mail.Authenticator() {
               protected PasswordAuthentication getPasswordAuthentication() {
-                return new PasswordAuthentication(username, password);
+                return new PasswordAuthentication("spoiledtomatillos@gmail.com",
+                        "SpoiledTomatillosSpoiledTomatillos");
               }
             });
 
@@ -35,10 +32,11 @@ public class CustomerEmail {
       Message message = new MimeMessage(session);
       message.setFrom(new InternetAddress("spoiledtomatillos@gmail.com"));
       message.setRecipients(Message.RecipientType.TO,
-              InternetAddress.parse("lixuanyu1993@gmail.com"));
-      message.setSubject("Testing Subject");
-      message.setText("Dear Spoiled Tomatillos User,"
-              + "\n\n No spam to my email, please!");
+              InternetAddress.parse("bergmann.t@husky.neu.edu"));
+      message.setSubject("Spoiled Tomatillos");
+      message.setText("Dear Travis Bergmann:" +
+              "\n\n This is Spoiled Tomatillos! Thanks for using our products! :)" +
+              "\n\n - Team 34");
 
       Transport.send(message);
 
