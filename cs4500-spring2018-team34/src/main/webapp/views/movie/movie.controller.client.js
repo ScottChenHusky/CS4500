0563c04d5765415d8c65da8e2f7725e38aa8404c
Travis Bergmann <bergmann.t@husky.neu.edu>
2018-03-13 14:01:49 -0400

    Added rating and review system

--- cs4500-spring2018-team34/src/main/webapp/views/movie/movie.controller.client.js
+++ cs4500-spring2018-team34/src/main/webapp/views/movie/movie.controller.client.js
@@ -2,7 +2,7 @@
 	angular.module('webapp').controller('movieDetailsController',
 			movieDetailsController);
 
-	function movieDetailsController($http, $routeParams) {
+	function movieDetailsController($http, $routeParams, $scope) {
 		var vm = this;
 		vm.movieId = $routeParams.mid;
 		vm.initMovie = initMovie;
@@ -21,6 +21,27 @@
 
 			}
 		}
+		
+		
+		$scope.rating = 0;
+		$scope.summary = "";
+		$scope.review = "";
+		
+		$scope.postReview = function() {
+			var url = '/api/movie/addComment'
+			var rR = {movieId:vm.movieId, rating:$scope.rating, summary:$scope.summary, review:$scope.review};
+			console.log(rR);
+			var data = JSON.stringify(rR);
+			console.log(data);
+			$http.post(url, data).then(function(response) {
+				if(response.message == "Already comment") {
+					alert("You've already reviewed this movie");
+				} else {
+					initMovie();
+				}
+			});
+		}
+		
 		initMovie();
 	}
 })();
