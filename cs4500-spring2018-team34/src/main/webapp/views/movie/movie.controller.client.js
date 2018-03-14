0f536191943b1d4e4d081ddd7ce3c85171bf82ee
Travis Bergmann <bergmann.t@husky.neu.edu>
2018-03-14 18:34:12 -0400

    Updated comment system front end

--- cs4500-spring2018-team34/src/main/webapp/views/movie/movie.controller.client.js
+++ cs4500-spring2018-team34/src/main/webapp/views/movie/movie.controller.client.js
@@ -6,14 +6,16 @@
 		var vm = this;
 		vm.movieId = $routeParams.mid;
 		vm.initMovie = initMovie;
-
+		vm.comments = [];
+		
 		function initMovie() {
             var url = '/api/movie/get?id=' + vm.movieId;
             return $http.get(url, vm.movieId)
 				.then(response, error);
 			function response(res) {
 				vm.movie = res.data.movie;
-
+				vm.comments = res.data.comment;
+				console.log(vm.comments);
 			}
 
 			function error(err) {
@@ -26,13 +28,13 @@
 		$scope.rating = 0;
 		$scope.summary = "";
 		$scope.review = "";
+		$scope.user = sessionStorage.getItem("currentUserId");
 		
 		$scope.postReview = function() {
-			var url = '/api/movie/addComment'
-			var rR = {movieId:vm.movieId, rating:$scope.rating, summary:$scope.summary, review:$scope.review};
-			console.log(rR);
+			var url = '/api/movie/addComment';
+			console.log($scope.user);
+			var rR = {movieId:vm.movieId, customerId:$scope.user, score:""+$scope.rating+"", review:$scope.review};	
 			var data = JSON.stringify(rR);
-			console.log(data);
 			$http.post(url, data).then(function(response) {
 				if(response.message == "Already comment") {
 					alert("You've already reviewed this movie");
